eba9568fe9a79ae69306393d178f149ba1493c88
plasmo310 <plasmo310@gmail.com>
2026-02-14 15:57:54 +0900

    add gltf model sample.

--- 04_load_assets/js/main_scene.js
+++ 04_load_assets/js/main_scene.js
@@ -1,5 +1,6 @@
 import * as THREE from "three";
 import { Elekibear } from "./elekibear.js";
+import { Star } from "./star.js";
 
 /**
  * Main Scene
@@ -31,11 +32,30 @@ export class MainScene {
     this.elekibear.root.position.set(0, -2, 0);
     this.elekibear.root.scale.set(2.5, 2.5, 2.5);
     this.scene.add(this.elekibear.root);
+
+    // create stars.
+    this.star = new Star();
+    this.star.root.scale.set(0.5, 0.5, 0.5);
+    this.star.root.position.set(3.5, 1, 0);
+    this.scene.add(this.star.root);
+
+    this.star_2 = new Star();
+    this.star_2.root.scale.set(0.35, 0.35, 0.35);
+    this.star_2.root.position.set(-3.5, -2, 0.5);
+    this.scene.add(this.star_2.root);
+
+    this.star_3 = new Star();
+    this.star_3.root.scale.set(0.25, 0.25, 0.25);
+    this.star_3.root.position.set(-3, 3, -1);
+    this.scene.add(this.star_3.root);
   }
 
   onUpdate(deltaTime) {
     // update objects.
     this.elekibear.onUpdate(deltaTime);
+    this.star.onUpdate(deltaTime);
+    this.star_2.onUpdate(deltaTime);
+    this.star_3.onUpdate(deltaTime);
   }
 
   onWindowResize(width, height) {
---
+++ 04_load_assets/js/star.js
@@ -0,0 +1,47 @@
+import * as THREE from "three";
+import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
+
+/**
+ * Star
+ */
+export class Star {
+  // asset paths.
+  MODEL_GLB_PATH = "public/models/SM_Star.glb";
+
+  constructor() {
+    // create root.
+    this.root = new THREE.Group();
+    this.root.name = "Star";
+
+    // load model.
+    this.loadModelAsync();
+  }
+
+  async loadModelAsync() {
+    // load mesh.
+    const loader = new GLTFLoader();
+    const model = await loader.loadAsync(this.MODEL_GLB_PATH);
+
+    // create material.
+    const material = new THREE.MeshPhongMaterial({
+      color: "yellow",
+    });
+
+    // replace materials.
+    model.scene.traverse((child) => {
+      if (child.isMesh) {
+        child.material = material;
+      }
+    });
+    model.scene.rotation.x = 90;
+
+    // add root.
+    this.root.clear();
+    this.root.add(model.scene);
+  }
+
+  onUpdate(deltaTime) {
+    // rotate.
+    this.root.rotation.z += 2 * deltaTime;
+  }
+}
