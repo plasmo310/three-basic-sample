50956954094215132105da44aacb4a0ada331d8f
plasmo310 <plasmo310@gmail.com>
2026-02-14 15:22:19 +0900

    play animation.

--- 04_load_assets/js/elekibear.js
+++ 04_load_assets/js/elekibear.js
@@ -5,31 +5,39 @@ import { FBXLoader } from "three/addons/loaders/FBXLoader.js";
  * Elekibear
  */
 export class Elekibear {
+  // asset paths.
+  MODEL_FBX_PATH = "public/models/SM_Elekibear.fbx";
+  BASE_COLOR_TEX_PATH = "public/textures/T_Elekibear.png";
+
   constructor() {
+    this.animMixer = null;
+
     // create root.
     this.root = new THREE.Group();
     this.root.name = "Elekibear";
 
+    // load model.
     this.loadModelAsync();
   }
 
   async loadModelAsync() {
     // エラー対処:
     // FBX内のテクスチャパスが無いとエラーになるので強制的に差し替え.
-    const TEXTURE_PATH = "public/textures/T_Elekibear.png";
+
     const manager = new THREE.LoadingManager();
     manager.setURLModifier((url) => {
-      if (/\.(png|jpe?g|tga|bmp|gif)$/i.test(url)) return TEXTURE_PATH;
+      if (/\.(png|jpe?g|tga|bmp|gif)$/i.test(url))
+        return this.BASE_COLOR_TEX_PATH;
       return url;
     });
 
     // load mesh.
     const loader = new FBXLoader(manager);
-    const mesh = await loader.loadAsync("public/models/SM_Elekibear.fbx");
+    const model = await loader.loadAsync(this.MODEL_FBX_PATH);
 
     // load texture.
     const baseColorTex = await new THREE.TextureLoader().loadAsync(
-      "/public/textures/T_Elekibear.png",
+      this.BASE_COLOR_TEX_PATH,
     );
     baseColorTex.colorSpace = THREE.SRGBColorSpace;
 
@@ -39,20 +47,31 @@ export class Elekibear {
     });
 
     // replace materials.
-    mesh.traverse((child) => {
+    model.traverse((child) => {
       if (child.isMesh) {
-        console.log(child.material);
         child.material = material;
       }
     });
 
+    // create animation mixer.
+    this.animMixer = new THREE.AnimationMixer(model);
+    if (model.animations.length > 0) {
+      // play animation.
+      const clip = model.animations[0];
+      const actionClip = this.animMixer.clipAction(clip);
+      actionClip.setLoop(THREE.LoopPingPong);
+      actionClip.play();
+    }
+
     // add root.
     this.root.clear();
-    this.root.add(mesh);
+    this.root.add(model);
   }
 
   onUpdate(deltaTime) {
-    // rotate.
-    this.root.rotation.y += 1 * deltaTime;
+    // play animation.
+    if (this.animMixer) {
+      this.animMixer.update(deltaTime);
+    }
   }
 }
